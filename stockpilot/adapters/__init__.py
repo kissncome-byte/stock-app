db9826bc19ea08a0c048e716b89c5e83838dc68b
kissncome-byte <kissncome@gmail.com>
2026-08-20 11:09:41 +0800

    Add files via upload

--- stockpilot/adapters/__init__.py
+++ stockpilot/adapters/__init__.py
@@ -1,1 +1,9 @@
+from .core_input_adapter import CoreInputAdapter, CoreInputAdapterError, DataQualityReport
+from .legacy_dict_adapter import LegacyDictAdapter
 
+__all__ = [
+    "CoreInputAdapter",
+    "CoreInputAdapterError",
+    "DataQualityReport",
+    "LegacyDictAdapter",
+]
